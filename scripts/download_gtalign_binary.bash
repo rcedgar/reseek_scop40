5d134ddfeada8e8603367c2e58797fa493c0723f
Robert Edgar <robert@drive5.com>
2024-06-04 08:42:23 -0700

    Add mkdir ../bin to download_gtalign_binary.bash

--- scripts/download_gtalign_binary.bash
+++ scripts/download_gtalign_binary.bash
@@ -9,6 +9,7 @@ mkdir -p ../gtalign_v0.15.0
 cd ../gtalign_v0.15.0
 wget https://github.com/minmarg/gtalign_alpha/archive/refs/tags/v0.15.0-alpha.tar.gz
 tar -xvf v0.15.0-alpha.tar.gz gtalign_alpha-0.15.0-alpha/Linux_installer_mp/bin/gtalign
-cp -v gtalign_alpha-0.15.0-alpha/Linux_installer_mp/bin/gtalign ../bin
 
+mkdir -p ../bin
+cp -v gtalign_alpha-0.15.0-alpha/Linux_installer_mp/bin/gtalign ../bin
 chmod +x ../bin/gtalign
